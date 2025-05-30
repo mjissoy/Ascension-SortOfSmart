c302e262a75a90000d496de55a8039d98ee2e66b
tyran <tyranus@live.dk>
2025-05-31 01:20:48 +0200

    Added Basic OreGen for Jade Ore

--- src/main/java/net/thejadeproject/ascension/datagen/DataGenerators.java
+++ src/main/java/net/thejadeproject/ascension/datagen/DataGenerators.java
@@ -34,6 +34,7 @@ public class DataGenerators {
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
+        generator.addProvider(event.includeServer(), new ModDatapackProvider(packOutput, lookupProvider));
 
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
---
+++ src/main/java/net/thejadeproject/ascension/datagen/ModDatapackProvider.java
@@ -0,0 +1,27 @@
+package net.thejadeproject.ascension.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.thejadeproject.ascension.AscensionCraft;
+import net.thejadeproject.ascension.worldgen.ModBiomeModifiers;
+import net.thejadeproject.ascension.worldgen.ModConfiguredFeatures;
+import net.thejadeproject.ascension.worldgen.ModPlacedFeatures;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+
+    public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(AscensionCraft.MOD_ID));
+    }
+}
