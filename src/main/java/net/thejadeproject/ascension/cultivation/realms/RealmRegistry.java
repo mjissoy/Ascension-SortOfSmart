4ee465c73d5afe09b263e55486a2cc12f8275dcb
clcment446 <125682659+clcment446@users.noreply.github.com>
2025-06-01 20:19:16 +0200

    Cultivation thingies

--- src/main/java/net/thejadeproject/ascension/cultivation/realms/RealmRegistry.java
+++ src/main/java/net/thejadeproject/ascension/cultivation/realms/RealmRegistry.java
@@ -7,6 +7,9 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.thejadeproject.ascension.AscensionCraft;
 
+import java.security.InvalidKeyException;
+import java.util.HashMap;
+
 public class RealmRegistry {
     public static final ResourceKey<Registry<Realm>> REALM_REGISTRY_KEY = ResourceKey.createRegistryKey(AscensionCraft.prefix("realms"));
     public static final ResourceKey<Registry<SubRealm>> SUB_REALM_REGISTRY_KEY = ResourceKey.createRegistryKey(AscensionCraft.prefix("subrealm"));
@@ -14,7 +17,7 @@ public class RealmRegistry {
     private static final DeferredRegister<SubRealm> SUB_REALMS = DeferredRegister.create(SUB_REALM_REGISTRY_KEY, AscensionCraft.MOD_ID);
 
     public static DeferredHolder<Realm, Realm> HEAVEN_QI_REFINING;
-    public static DeferredHolder<Realm,Realm> EARTH_QI_REFINING;
+    public static DeferredHolder<Realm, Realm> EARTH_QI_REFINING;
     public static DeferredHolder<SubRealm, SubRealm> QI_REFINING_1;
     public static DeferredHolder<SubRealm, SubRealm> QI_REFINING_2;
     public static DeferredHolder<SubRealm, SubRealm> QI_REFINING_3;
@@ -32,5 +35,37 @@ public class RealmRegistry {
     public static void register(IEventBus modEventBus) {
         REALMS.register(modEventBus);
         SUB_REALMS.register(modEventBus);
+    }
+
+    static final HashMap<String, Realm> REALM_MAP = new HashMap<>();
+    static final HashMap<String, SubRealm> SUB_REALM_MAP = new HashMap<>();
+
+    static void initialize() {
+        REALMS.getEntries().forEach(entry -> {
+            REALM_MAP.put(entry.getId().getPath(), entry.value());
+        });
+
+        SUB_REALMS.getEntries().forEach(entry -> {
+            SUB_REALM_MAP.put(entry.getId().getPath(), entry.value());
+        });
+
+    }
+
+    public static Realm getRealmFromKey(String key) throws InvalidKeyException {
+        var k = REALM_MAP.get(key);
+        if (k == null) {
+            throw (new InvalidKeyException("ERROR !\nKEY IS NOT IN REGISTRY!"));
+        } else {
+            return k;
+        }
+    }
+
+    public static SubRealm getSubRealmFromKey(String key) throws InvalidKeyException {
+        var k = SUB_REALM_MAP.get(key);
+        if (k == null) {
+            throw (new InvalidKeyException("ERROR !\nKEY IS NOT IN REGISTRY!"));
+        } else {
+            return k;
+        }
     }
 }
