5d228ac8f88cdf229157ad1821456dd274e59e55
MrEizy <MrEizy@hotmail.com>
2025-11-26 16:09:54 +0100

    Stuff and Things

--- src/main/java/net/thejadeproject/ascension/AscensionCraft.java
+++ src/main/java/net/thejadeproject/ascension/AscensionCraft.java
@@ -55,6 +55,7 @@ import net.thejadeproject.ascension.util.KeyBindHandler;
 import net.thejadeproject.ascension.util.ModAttachments;
 import net.thejadeproject.ascension.util.ModAttributes;
 import net.thejadeproject.ascension.util.ToolTips.ToolTipManager;
+import net.thejadeproject.ascension.villager.ModVillagers;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -124,6 +125,9 @@ public class AscensionCraft {
         ModSkills.register(modEventBus);
         ModTechniques.register(modEventBus);
         ModDao.register(modEventBus);
+
+        ModVillagers.VILLAGER_PROFESSIONS.register(modEventBus);
+        ModVillagers.POI_TYPES.register(modEventBus);
 
         NeoForge.EVENT_BUS.addListener(this::registerCommands);
     }
---
+++ src/main/java/net/thejadeproject/ascension/villager/ModVillagers.java
@@ -0,0 +1,36 @@
+package net.thejadeproject.ascension.villager;
+
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.block.SoundType;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.thejadeproject.ascension.AscensionCraft;
+import net.thejadeproject.ascension.blocks.ModBlocks;
+
+public class ModVillagers {
+    public static final DeferredRegister<PoiType> POI_TYPES =
+            DeferredRegister.create(BuiltInRegistries.POINT_OF_INTEREST_TYPE, AscensionCraft.MOD_ID);
+    public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS =
+            DeferredRegister.create(BuiltInRegistries.VILLAGER_PROFESSION, AscensionCraft.MOD_ID);
+
+
+    public static final Holder<PoiType> HERBALIST_POI = POI_TYPES.register("herbalist_poi",
+            () -> new PoiType(ImmutableSet.copyOf(ModBlocks.PILL_CAULDRON_HUMAN_LOW.get().getStateDefinition().getPossibleStates()), 1, 1));
+
+    public static final Holder<VillagerProfession> HERBALIST = VILLAGER_PROFESSIONS.register("herbalist",
+            () -> new VillagerProfession("herbalist", holder -> holder.value() == HERBALIST_POI.value(),
+                    poiTypeHolder -> poiTypeHolder.value() == HERBALIST_POI.value(), ImmutableSet.of(), ImmutableSet.of(),
+                    SoundType.MOSS.getPlaceSound()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        POI_TYPES.register(eventBus);
+        VILLAGER_PROFESSIONS.register(eventBus);
+    }
+
+}
