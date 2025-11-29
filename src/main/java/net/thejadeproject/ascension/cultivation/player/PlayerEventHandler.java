84f1bba3303f4a67761ed06ca5f6e038321efaef
MrEizy <MrEizy@hotmail.com>
2025-11-29 21:43:35 +0100

    Fixed visual bug

--- src/main/java/net/thejadeproject/ascension/cultivation/player/PlayerEventHandler.java
+++ src/main/java/net/thejadeproject/ascension/cultivation/player/PlayerEventHandler.java
@@ -1,6 +1,8 @@
 package net.thejadeproject.ascension.cultivation.player;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.neoforge.network.PacketDistributor;
 import net.thejadeproject.ascension.cultivation.player.data_attachements.CultivationData;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Player;
@@ -21,6 +23,7 @@ import net.thejadeproject.ascension.AscensionCraft;
 import net.thejadeproject.ascension.cultivation.player.data_attachements.PlayerData;
 import net.thejadeproject.ascension.entity.custom.AscensionSkillEntity;
 import net.thejadeproject.ascension.events.custom.GatherEfficiencyModifiersEvent;
+import net.thejadeproject.ascension.network.clientBound.SyncPathDataPayload;
 import net.thejadeproject.ascension.progression.techniques.ITechnique;
 import net.thejadeproject.ascension.registries.AscensionRegistries;
 import net.thejadeproject.ascension.util.ModAttachments;
@@ -111,10 +114,14 @@ public class PlayerEventHandler {
             if(player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").technique.equals("ascension:none")) return;
             player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").pathProgress=0;
             player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").stabilityCultivationTicks=0;
+        }
+    }
 
+    @SubscribeEvent
+    public static void respawnEvent(PlayerEvent.PlayerRespawnEvent event) {
+        var player = event.getEntity();
+        PacketDistributor.sendToPlayer((ServerPlayer) player, SyncPathDataPayload.fromPathData(player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body")));
         }
     }
 
 
-
-}
