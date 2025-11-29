25befa70f6d4d8b3a0d667eed9debc7fe1913bac
MrEizy <MrEizy@hotmail.com>
2025-11-29 21:33:23 +0100

    Dying with body cult resets progress

--- src/main/java/net/thejadeproject/ascension/cultivation/player/PlayerEventHandler.java
+++ src/main/java/net/thejadeproject/ascension/cultivation/player/PlayerEventHandler.java
@@ -1,6 +1,7 @@
 package net.thejadeproject.ascension.cultivation.player;
 
 import net.minecraft.resources.ResourceLocation;
+import net.thejadeproject.ascension.cultivation.player.data_attachements.CultivationData;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -17,8 +18,10 @@ import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.thejadeproject.ascension.AscensionCraft;
+import net.thejadeproject.ascension.cultivation.player.data_attachements.PlayerData;
 import net.thejadeproject.ascension.entity.custom.AscensionSkillEntity;
 import net.thejadeproject.ascension.events.custom.GatherEfficiencyModifiersEvent;
+import net.thejadeproject.ascension.progression.techniques.ITechnique;
 import net.thejadeproject.ascension.registries.AscensionRegistries;
 import net.thejadeproject.ascension.util.ModAttachments;
 import net.thejadeproject.ascension.util.ModTags;
@@ -103,9 +106,11 @@ public class PlayerEventHandler {
 
 
     @SubscribeEvent
-    public static void livingDeathEvent(LivingDeathEvent event){
-        if(event.getEntity() != null && event.getEntity() instanceof Player player) {
-
+    public static void livingDeathEvent(LivingDeathEvent event) {
+        if (event.getEntity() != null && event.getEntity() instanceof Player player) {
+            if(player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").technique.equals("ascension:none")) return;
+            player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").pathProgress=0;
+            player.getData(ModAttachments.PLAYER_DATA).getCultivationData().getPathData("ascension:body").stabilityCultivationTicks=0;
 
         }
     }
