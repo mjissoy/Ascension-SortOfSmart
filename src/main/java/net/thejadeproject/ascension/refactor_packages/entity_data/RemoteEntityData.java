15a85d0fcf3fda97702198aadce11bc34e1f8439
mjissoy <mjissoy@gmail.com>
2026-04-19 21:53:06 +1000

    fun

--- src/main/java/net/thejadeproject/ascension/refactor_packages/entity_data/RemoteEntityData.java
+++ src/main/java/net/thejadeproject/ascension/refactor_packages/entity_data/RemoteEntityData.java
@@ -266,7 +266,7 @@ public class RemoteEntityData implements IEntityData{
 
     @Override
     public boolean setTechnique(ResourceLocation path) {
-        EntityDataManager.getEntityData(getWatchedEntityData()).setTechnique(path)
+        return EntityDataManager.getEntityData(getWatchedEntityData()).setTechnique(path);
     }
 
     @Override
